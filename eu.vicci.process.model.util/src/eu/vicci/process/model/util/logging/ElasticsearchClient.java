4ed48db745b015f0ef6642d79841ca9dd1b77602
andre <andre.kuehnie@gmail.com>
2016-12-30 13:13:04 +0100

    fixes #1

--- eu.vicci.process.model.util/src/eu/vicci/process/model/util/logging/ElasticsearchClient.java
+++ eu.vicci.process.model.util/src/eu/vicci/process/model/util/logging/ElasticsearchClient.java
@@ -1,10 +1,20 @@
 package eu.vicci.process.model.util.logging;
 
+import feign.Param;
 import feign.RequestLine;
 
 public interface ElasticsearchClient {
 
-	@RequestLine("POST /mapek-2016-12/timer/")
-	void postDecember2016(String timer);
+	/**
+	 * Posts the log to elastic search.
+	 * 
+	 * @param year
+	 *            the year e.g. 2016 as string
+	 * @param month
+	 *            the month e.g. 06 as string
+	 * @param timer
+	 */
+	@RequestLine("POST /mapek-{year}-{month}/timer/")
+	void post(@Param("year") String year, @Param("month") String month, String timer);
 
 }
--- eu.vicci.process.model.util/src/eu/vicci/process/model/util/logging/LoggingManager.java
+++ eu.vicci.process.model.util/src/eu/vicci/process/model/util/logging/LoggingManager.java
@@ -1,5 +1,7 @@
 package eu.vicci.process.model.util.logging;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -71,32 +73,63 @@ public class LoggingManager {
 		recentStateChanges.add(message);
 	}
 
-	private void report(IStateChangeMessage message) {
-		final MetricRegistry r = registry.get();
-		
-		final String model = "model-" + message.getProcessId();
-		final String step  =  model   + ".step-" + message.getModelId();
-		final String stepInstance  = step  + ".step-instance-"  + message.getInstanceId();
-		
+	private void report(IStateChangeMessage message) {		
 		if (State.EXECUTING == message.getState()) {
-			TimerWithStart timer = TimerWithStart.create(r.timer(stepInstance).time());
-			timer.model = message.getProcessId();
-			timer.step = message.getModelId();
-			timer.stepInstance = message.getInstanceId();
-			timer.name = "proteus." + timer.stepInstance;
-			timer.process = message.getProcessName();
-			timerContexts.put(stepInstance, timer);
+			logExecutingIfNeeded(message);
+		}		
+		else if (State.EXECUTED == message.getState()) { 
+			logExecuted(message);
 		}
+	}
+	
+	private void logExecuted(IStateChangeMessage message){
+		LogIdentifier logId = new LogIdentifier(message);
+		TimerWithStart timer = timerContexts.remove(logId.stepInstance).stop();
 		
-		if (State.EXECUTED == message.getState()) { 
-			TimerWithStart timer = timerContexts.remove(stepInstance).stop();
-			
-			try {
-				esClient.postDecember2016(objectMapper.writeValueAsString(timer));
-			} catch (JsonProcessingException e) {
-				e.printStackTrace();
-			}
-		}
+		Calendar cal = getCurrentCalendar();
+		final int iMonth = cal.get(Calendar.MONTH) + 1; //zero based month
+		final int iYear = cal.get(Calendar.YEAR);
+		final String month = iMonth < 10 ? "0" + String.valueOf(iMonth) : String.valueOf(iMonth);
+		final String year = String.valueOf(iYear);
+		
+		try {
+			esClient.post(year, month, objectMapper.writeValueAsString(timer));
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}		
+	}
+	
+	private void logExecutingIfNeeded(IStateChangeMessage message){
+		final LogIdentifier logId = new LogIdentifier(message);		
+		if(timerContexts.containsKey(logId.stepInstance)) //time already started
+			return;
+		
+		final MetricRegistry r = registry.get();			
+		TimerWithStart timer = TimerWithStart.create(r.timer(logId.stepInstance).time());
+		timer.model = message.getProcessId();
+		timer.step = message.getModelId();
+		timer.stepInstance = message.getInstanceId();
+		timer.name = "proteus." + timer.stepInstance;
+		timer.process = message.getProcessName();
+		timerContexts.put(logId.stepInstance, timer);
+	}
+	
+	private static Calendar getCurrentCalendar(){
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(new Date());
+		return cal;
+	}
+	
+	private class LogIdentifier {
+		private final String model;
+		private final String step;
+		private final String stepInstance;
+		
+		public LogIdentifier(IStateChangeMessage message) {
+			model = "model-" + message.getProcessId();
+			step  =  model   + ".step-" + message.getModelId();
+			stepInstance  = step  + ".step-instance-"  + message.getInstanceId();				
+		}				
 	}
 
 }
