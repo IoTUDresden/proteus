81e682fea713405060ab78559ac2fbb17a0416b1
andre <andre.kuehnie@gmail.com>
2017-01-10 17:56:18 +0100

    log all finished processes

--- eu.vicci.process.model.util/src/eu/vicci/process/model/util/logging/LoggingManager.java
+++ eu.vicci.process.model.util/src/eu/vicci/process/model/util/logging/LoggingManager.java
@@ -20,32 +20,31 @@ import eu.vicci.process.model.util.messages.core.IStateChangeMessage;
 import feign.Feign;
 
 public class LoggingManager {
-	
+
 	private static LoggingManager lm;
 	private Map<String, InstanceLogger> loggers = new ConcurrentHashMap<>();
 	private List<IStateChangeMessage> recentStateChanges = new CopyOnWriteArrayList<>();
-	
+
 	private Optional<MetricRegistry> registry = Optional.empty();
-	
+
 	private Map<String, TimerWithStart> timerContexts = new ConcurrentHashMap<>();
 	private ElasticsearchClient esClient;
 	private ObjectMapper objectMapper;
-	
+
 	public static synchronized LoggingManager getInstance() {
 		if (lm == null) {
 			lm = new LoggingManager();
-		}		
+		}
 		return lm;
 	}
-	
+
 	private LoggingManager() {
-		String esHost = ConfigurationManager.getInstance()
-			.getConfigAsString(ConfigProperties.ELASTICSEARCH_HOST);
-		
+		String esHost = ConfigurationManager.getInstance().getConfigAsString(ConfigProperties.ELASTICSEARCH_HOST);
+
 		objectMapper = new ObjectMapper();
 		esClient = Feign.builder().target(ElasticsearchClient.class, "http://" + esHost);
 	}
-	
+
 	public void setMetricsReporter(MetricRegistry registry) {
 		this.registry = Optional.ofNullable(registry);
 	}
@@ -61,50 +60,51 @@ public class LoggingManager {
 	public List<IStateChangeMessage> getRecentStateChanges() {
 		return recentStateChanges;
 	}
-	
-	public void createLoggingForInstance(ProcessInstance processInstance){
+
+	public void createLoggingForInstance(ProcessInstance processInstance) {
 		InstanceLogger il = new InstanceLogger(processInstance, new CopyOnWriteArrayList<IStateChangeMessage>());
-		loggers.put(processInstance.getInstanceId(), il);		
+		loggers.put(processInstance.getInstanceId(), il);
 	}
-	
-	public void logStateMessage(IStateChangeMessage message){
-		if (registry.isPresent()) report(message);
+
+	public void logStateMessage(IStateChangeMessage message) {
+		if (registry.isPresent())
+			report(message);
 		loggers.get(message.getProcessInstanceId()).logStateMessage(message);
 		recentStateChanges.add(message);
 	}
 
-	private void report(IStateChangeMessage message) {		
+	private void report(IStateChangeMessage message) {
 		if (State.EXECUTING == message.getState()) {
 			logExecutingIfNeeded(message);
-		}		
-		else if (State.EXECUTED == message.getState()) { 
+		} else if (isFinishedState(message)) {
 			logExecuted(message);
 		}
 	}
-	
-	private void logExecuted(IStateChangeMessage message){
+
+	private void logExecuted(IStateChangeMessage message) {
 		LogIdentifier logId = new LogIdentifier(message);
 		TimerWithStart timer = timerContexts.remove(logId.stepInstance).stop();
-		
+
 		Calendar cal = getCurrentCalendar();
-		final int iMonth = cal.get(Calendar.MONTH) + 1; //zero based month
+		final int iMonth = cal.get(Calendar.MONTH) + 1; // zero based month
 		final int iYear = cal.get(Calendar.YEAR);
 		final String month = iMonth < 10 ? "0" + String.valueOf(iMonth) : String.valueOf(iMonth);
 		final String year = String.valueOf(iYear);
-		
+
 		try {
 			esClient.post(year, month, objectMapper.writeValueAsString(timer));
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
-		}		
+		}
 	}
-	
-	private void logExecutingIfNeeded(IStateChangeMessage message){
-		final LogIdentifier logId = new LogIdentifier(message);		
-		if(timerContexts.containsKey(logId.stepInstance)) //time already started
+
+	private void logExecutingIfNeeded(IStateChangeMessage message) {
+		final LogIdentifier logId = new LogIdentifier(message);
+		if (timerContexts.containsKey(logId.stepInstance)) // time already
+															// started
 			return;
-		
-		final MetricRegistry r = registry.get();			
+
+		final MetricRegistry r = registry.get();
 		TimerWithStart timer = TimerWithStart.create(r.timer(logId.stepInstance).time());
 		timer.model = message.getProcessId();
 		timer.step = message.getModelId();
@@ -113,23 +113,34 @@ public class LoggingManager {
 		timer.process = message.getProcessName();
 		timerContexts.put(logId.stepInstance, timer);
 	}
-	
-	private static Calendar getCurrentCalendar(){
+
+	private static Calendar getCurrentCalendar() {
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(new Date());
 		return cal;
 	}
-	
+
+	private boolean isFinishedState(IStateChangeMessage message) {
+		switch (message.getState()) {
+		case EXECUTED:
+		case FAILED:
+		case ESCALATED:
+			return true;
+		default:
+			return false;
+		}
+	}
+
 	private class LogIdentifier {
 		private final String model;
 		private final String step;
 		private final String stepInstance;
-		
+
 		public LogIdentifier(IStateChangeMessage message) {
 			model = "model-" + message.getProcessId();
-			step  =  model   + ".step-" + message.getModelId();
-			stepInstance  = step  + ".step-instance-"  + message.getInstanceId();				
-		}				
+			step = model + ".step-" + message.getModelId();
+			stepInstance = step + ".step-instance-" + message.getInstanceId();
+		}
 	}
 
 }
