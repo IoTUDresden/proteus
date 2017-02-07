46af5c0d5db39870ad7d11c4e1d403ff99a066bd
andiikaa <andre.kuehnie@gmail.com>
2017-02-07 17:20:27 +0100

    fixing deployment

--- deploy.sh
+++ deploy.sh
@@ -35,4 +35,4 @@ chmod a+x update-directory-index.sh
 
 git add -A
 git commit -m "deploy p2 and maven repo"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" gh-pages
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" gh-pages > /dev/null 2>&1
