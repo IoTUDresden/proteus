246e856c2ac1823185753f574cb6ab72c97f0339
andiikaa <andre.kuehnie@gmail.com>
2017-11-16 15:20:02 +0100

    first try to release with travis

--- deploy.sh
+++ deploy.sh
@@ -1,38 +1,95 @@
 #!/bin/bash -e
 
+# This script will deploy assamble all stuff from the build folders
+# and push artifacts to the p2 and maven repository.
+# Editor builds and vicci-runtime builds are copied to a folder,
+# which can be published to Github releases via the travis provider: releases.
+
 git config --global user.name "Travis CI"
 git config --global user.email "someEmail@email.org"
 
-travisBuildPath="/home/travis/build"
+# the base path where travis will put build artifacts
+#TRAVIS_BUILD_PATH="/home/travis/build"
+# the path for the binary p2 repo
+P2_TARGET_PATH="sites/p2/target/repository"
+# the path for the binary maven repo
+MAVEN_TARGET_PATH="target/repo"
+# the path where the p2 repo should be deployed
+P2_DEPLOY_PATH="p2/latest"
+# the path where the p2 repo should be deployed
+MAVEN_DEPLOY_PATH="maven/snapshot"
+# Releases path. All files in here are deployed to gh releases.
+#RELEASES_PATH="bin_release"
+# Path where the editor zip files located (maven has already packed them)
+EDITOR_PATH="products/editor/target/products"
+# proteus shaded path
+PROTEUS_SHADED_PATH="products/proteus"
 
-p2TargetPath="sites/p2/target/repository"
-mavenTargetPath="target/repo"
+# user name for travis user
+USER_NAME="IoTUDresden"
+# user name for github user (where the repo is from)
+GH_USER_NAME="IoTUDresden"
+# gh project name for the repo (p2 and maven)
+REPO_NAME="proteus-repo"
+# repo is cloned to this folder
+TMP_REPO_NAME="proteus-repo"
+# name for the proteus project
+PROTEUS_NAME="proteus"
 
-p2DeployPath="p2/latest"
-mavenDeployPath="maven/snapshot"
 
-userName="IoTUDresden"
-ghUserName="IoTUDresden"
-repoName="proteus-repo"
-tmpRepoName="proteus-repo"
-proteusRepo="proteus"
 
-git clone https://github.com/$ghUserName/$repoName.git $travisBuildPath/$userName/$tmpRepoName
-cd $travisBuildPath/$userName/$tmpRepoName
+################################
+# Preparing the release folder #
+################################
+
+echo "prepare Github Releases..."
+
+mkdir -p "$TRAVIS_BUILD_PATH/$USER_NAME/$RELEASES_PATH"
+
+echo "copy editor build to release..."
+
+cd $TRAVIS_BUILD_PATH/$USER_NAME/$EDITOR_PATH/
+find . -name '*.zip' -exec mv {} "$TRAVIS_BUILD_PATH/$USER_NAME/$RELEASES_PATH" \;
+
+echo "assamble vicci-runtime and copy to release..."
+
+cd $TRAVIS_BUILD_PATH/$USER_NAME/$PROTEUS_SHADED_PATH/
+chmod a+x assamble.sh
+./assamble.sh
+
+cp -r $TRAVIS_BUILD_PATH/$USER_NAME/$PROTEUS_SHADED_PATH/vicci-runtime.zip $TRAVIS_BUILD_PATH/$USER_NAME/$RELEASES_PATH/vicci-runtime.zip
+
+###########################
+# Deploy the repositories #
+###########################
+
+echo "cloning the target p2 and maven repository..."
+
+git clone https://github.com/$GH_USER_NAME/$REPO_NAME.git $TRAVIS_BUILD_PATH/$USER_NAME/$TMP_REPO_NAME
+cd $TRAVIS_BUILD_PATH/$USER_NAME/$TMP_REPO_NAME
 git checkout gh-pages
 
-rm -r "$p2DeployPath"
-rm -r "$mavenDeployPath"
+echo "copy binaries to repository..."
 
-mkdir -p "$p2DeployPath"
-mkdir -p "$mavenDeployPath"
 
-cp -r $travisBuildPath/$userName/$proteusRepo/$p2TargetPath/. $travisBuildPath/$userName/$tmpRepoName/$p2DeployPath/
-cp -r $travisBuildPath/$userName/$proteusRepo/$mavenTargetPath/. $travisBuildPath/$userName/$tmpRepoName/$mavenDeployPath/
+# todo we should only override snapshots or the same version, at least for the maven part.
+rm -r "$P2_DEPLOY_PATH"
+rm -r "$MAVEN_DEPLOY_PATH"
+
+mkdir -p "$P2_DEPLOY_PATH"
+mkdir -p "$MAVEN_DEPLOY_PATH"
+
+cp -r $TRAVIS_BUILD_PATH/$USER_NAME/$PROTEUS_NAME/$P2_TARGET_PATH/. $TRAVIS_BUILD_PATH/$USER_NAME/$TMP_REPO_NAME/$P2_DEPLOY_PATH/
+cp -r $TRAVIS_BUILD_PATH/$USER_NAME/$PROTEUS_NAME/$MAVEN_TARGET_PATH/. $TRAVIS_BUILD_PATH/$USER_NAME/$TMP_REPO_NAME/$MAVEN_DEPLOY_PATH/
+
+echo "update directory index of the repository..."
 
 chmod a+x update-directory-index.sh
 ./update-directory-index.sh
 
+echo "commit and push the repository..."
+
 git add -A
 git commit -m "deploy p2 and maven repo"
+# this does a silent commit and also does not show any errors. So the token will should not be vissible in the travis log
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" gh-pages > /dev/null 2>&1
---
+++ products/proteus/assamble.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+VICCI_RUNTIME_PATH="vicci-runtime"
+VICCI_RUNTIME_BIN_PATH="bin"
+VICCI_RUNTIME_BUILD_PATH="target"
+SHADED_JAR_NAME="eu.vicci.proteus-1.0.0-SNAPSHOT.jar"
+
+mkdir -p "$VICCI_RUNTIME_PATH/$VICCI_RUNTIME_BIN_PATH"
+
+cp vicci-runtime.bat $VICCI_RUNTIME_PATH/vicci-runtime.bat
+cp vicci-runtime.sh $VICCI_RUNTIME_PATH/vicci-runtime.sh
+cp $VICCI_RUNTIME_BUILD_PATH/$SHADED_JAR_NAME $VICCI_RUNTIME_PATH/$VICCI_RUNTIME_BIN_PATH/$SHADED_JAR_NAME
+
+zip -r vicci-runtime.zip vicci-runtime
